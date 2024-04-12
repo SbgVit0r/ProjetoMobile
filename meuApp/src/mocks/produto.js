71fe48a6d1d3da8954a71a612c14a440f51fc640
Vitor Sbeghi <126426164+SbgVit0r@users.noreply.github.com>
2024-04-11 22:34:05 -0300

    CHanges

--- meuApp/src/mocks/produto.js
+++ meuApp/src/mocks/produto.js
@@ -1,4 +1,6 @@
-import Logo from '../../assets/logo.png';
+import jogadores_jogo from '../../assets/jogadores.png';
+import idiomas_jogo from '../../assets/idiomas.png';
+import genero_jogo from '../../assets/genero.png';
 
 const produto = {
     topo: {
@@ -6,11 +8,27 @@ const produto = {
     },
     detalhes: {
         nome: "Kirby and The Forgotten Land",
-        logo: Logo,
         detalhes: "Embarque em uma aventura totalmente nova como a poderosa bolinha, Kirby. Explore livremente em fases 3D enquanto descobre um mundo misterioso com estruturas abandonadas de uma civilização passada - como um shopping center?! Copie as habilidades dos inimigos, como o novo Drill e Ranger, e use-as para atacar, explorar seus arredores e salvar os Waddle Dees sequestrados do feroz Beast Pack ao lado do misterioso Elfilin.",
         preco: "R$ 299,90",
         botao: "Adicionar na lista de desejos",
     },
+    itens:{
+        titulo: "Especificações do jogo",
+        lista: [
+            {
+                jogadores: "2 Jogadores Co-Op",
+                imagem: jogadores_jogo,
+            },
+            {
+                idiomas: "Alemão, Chinês Simplificado, Chinês Tradicional, Coreano, Espanhol, Francês, Holandês, Inglês, Italiano, Japonês",
+                imagem: idiomas_jogo,
+            },
+            {
+                genero: "Plataforma",
+                imagem: genero_jogo
+            }
+        ]
+    }
 }
 
 export default produto;
--- meuApp/src/telas/Produto/index.js
+++ meuApp/src/telas/Produto/index.js
@@ -1,13 +1,26 @@
-import React from "react"
+import React from "react";
+import { FlatList, View, Image, Text} from "react-native";
 
 import Topo from './componentes/Topo';
 import Detalhes from './componentes/Detalhes';
 
-export default function Produto({topo, detalhes}){
+export default function Produto({topo, detalhes, itens}){
+    const renderItem = ({item:{nome, imagem}}) =><View key={nome}>
+                                <Image source={imagem}/>
+                                <Text>{nome}</Text>
+                            </View>
+
     return <>
+        <FlatList
+            data={itens.lista}
+            renderItem={renderItem}
+            keyExtractor={({nome})=>{nome}}
+        />
+        
         <Topo {...topo}/>
         <Detalhes {...detalhes}/>
     </>
 }
+
 
 
