263829bf1a742d1e86b9e1a09e4d6d2838189510
Vitor Sbeghi <126426164+SbgVit0r@users.noreply.github.com>
2024-04-16 22:20:52 -0300

    Aula React

--- meuApp/src/telas/Produto/index.js
+++ meuApp/src/telas/Produto/index.js
@@ -3,6 +3,7 @@ import { FlatList, View, Image, Text} from "react-native";
 
 import Topo from './componentes/Topo';
 import Detalhes from './componentes/Detalhes';
+import Item from "./componentes/Item";
 
 export default function Produto({topo, detalhes, itens}){
     const renderItem = ({item:{nome, imagem}}) =><View key={nome}>
@@ -13,12 +14,18 @@ export default function Produto({topo, detalhes, itens}){
     return <>
         <FlatList
             data={itens.lista}
-            renderItem={renderItem}
-            keyExtractor={({nome})=>{nome}}
+            renderItem={Item}
+            keyExtractor={itens.lista.nome}
+            ListHeaderComponent ={() =>{
+                return <>      
+                    <Topo {...topo}/>
+                    <Detalhes {...detalhes}/>
+                </>
+            }}
         />
         
-        <Topo {...topo}/>
-        <Detalhes {...detalhes}/>
+
+
     </>
 }
 
