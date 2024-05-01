1e4e6f2f7429881e64164db8c05b01c1938f426f
Vitor Sbeghi <126426164+SbgVit0r@users.noreply.github.com>
2024-04-30 22:26:05 -0300

    Aula

--- meuApp/src/telas/Produto/index.js
+++ meuApp/src/telas/Produto/index.js
@@ -1,13 +1,15 @@
 import React from "react";
-import {FlatList} from "react-native";
+import {FlatList, StyleSheet} from "react-native";
 
 import Topo from './componentes/Topo';
 import Detalhes from './componentes/Detalhes';
 import Item from "./componentes/Item";
+import Texto from "../../Componentes/Texto";
 
 export default function Produto({topo, detalhes, itens}){
 
     return <>
+        <Texto style={styles.titulo}>{itens.titulo}</Texto>
         <FlatList
             data={itens.lista}
             renderItem={Item}
@@ -24,3 +26,12 @@ export default function Produto({topo, detalhes, itens}){
 
     </>
 }
+
+const styles = StyleSheet.create({
+    titulo : {
+        fontSize: 26,
+        color: "black",
+        fontWeight: "bold",
+        textAlign: "center",
+    }
+})
